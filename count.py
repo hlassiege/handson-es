52541bfe0c6255d98184f826e28959c4b39347fa
hlassiege <h.lassiege@gmail.com>
2013-04-16 16:46:38 +0200

    remove files that should not be here...

--- count.py
+++
@@ -1,6 +0,0 @@
-import requests, json
-
-BASE_PATH = "http://localhost:9200/"
-
-r = requests.get(BASE_PATH+"/library/movie/_count")
-print "Number of document in the index : %s" % r.json()['count']
--- delete_index.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-BASE_PATH = "http://localhost:9200/"
-
-
-r = requests.delete(BASE_PATH+"/library/movie/")
-
-r = requests.get(BASE_PATH+"/library/movie/_count")
-print "Number of document in the index : %s" % r.json()['count']
